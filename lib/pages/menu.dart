903c21b27dbf214619a2115bc9ec2dc7b403c30b
Pizzi-F <pizzi480@gmail.com>
2022-06-13 20:35:26 -0300

    fodase2

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -45,7 +45,7 @@ class Menu extends StatefulWidget {
                           content: Text('Você já Possui Carona Cadastrada!')),
                     );
                   } else {
-                    MinhaCarona.pegou_carona = tru;
+                    MinhaCarona.pegou_carona = true;
                     DarCarona.caronas[index].vagas--;
                     MinhaCarona.AddMinhaCarona(
                       DarCarona.caronas[index].nome,
